d5397ccb38a515383dcc8ea3a4f55097d86c5039
janghyunmoon <abc9529@naver.com>
2019-02-01 21:16:38 +0900

    filenamepolicy

--- 06_HelloMVC/src/common/rename/MyFileRenamePolicy.java
+++ 06_HelloMVC/src/common/rename/MyFileRenamePolicy.java
@@ -23,21 +23,23 @@ public class MyFileRenamePolicy implements FileRenamePolicy {
 			String oldName = oldfile.getName(); 
 			String ext = ""; // 확장자 보관용
 			int dot = oldName.lastIndexOf(".");
+			// lastIndexOf()로 못찾으면 -1 반환
 			if (dot > -1) {
-				ext = oldName.substring(dot); // dot가 '.'의 위치를 보유, . 있는 위치부터 뒤의 내용 다 자름
+				ext = oldName.substring(dot); // dot가 '.'의 위치를 보유, . 있는 위치부터 뒤의 내용 다 자름 ex) test.hwp => ".hwp"
 			}
 			// 새파일명 생성
 			String newName = sdf.format(new Date(currentTime)) + "_" + randomNum + ext;
 			
+			// 부모디렉토리 경로 + newName 
 			newFile = new File(oldfile.getParent(), newName);
-		} while(! createNewFile(newFile));
+		} while(! createNewFile(newFile)); // 생성을 정상적으로 완료하면 false가 되서 종료
 		
 		return newFile;
 	}
 
 	private boolean createNewFile(File newFile) {
 		try {
-			
+			// 디렉토리가 없거나 중복값이 있는 경우 생성이 안됨
 			return newFile.createNewFile();
 		} catch (IOException e) {
 			return false;
